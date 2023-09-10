ec388c91b00bb896e9769b93c954aa7f938615da
Lira11io <Lira11pm@mail.ru>
2023-09-10 23:48:39 +0300

    попытка сбрасывать состояние перевода пр пролистывании

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -20,21 +20,36 @@ export default function Slider() {
   //создаем переменную для объекта массива
   const object = words[currentCardIndex];
 
+  //создаем состояние для кнопки перевода
+  //const [translated, setTranslated] = useState(true);
+
+  //создаем функцию, которая срабатывает на кнопке "Показать перевод"
+  /*  function showTranslation() {
+    setTranslated(!translated);
+  } */
+
   //создаем функцию, которая увеличивает индекс на один и листает карточку вправо
   const goToNextCard = () => {
-    if (currentCardIndex < arr.length - 1) {
-      setCurrentCardIndex(currentCardIndex + 1);
-    } else {
-      setCurrentCardIndex(0);
-    }
+    setCurrentCardIndex((prevIndex) => {
+      if (prevIndex === arr.length - 1) {
+        //если текущая карточка последняя
+        return 0; //перейти на первую карточку
+      } else {
+        return prevIndex + 1; //увеличить индекс на 1
+      }
+    });
   };
+
   //создаем функцию, которая уменьшает индекс на один и листает карточку влево
   const goToPreviousCard = () => {
-    if (currentCardIndex > 0) {
-      setCurrentCardIndex(currentCardIndex - 1);
-    } else {
-      setCurrentCardIndex(arr.length - 1);
-    }
+    setCurrentCardIndex((prevIndex) => {
+      if (prevIndex === 0) {
+        //если текущая карточка первая
+        return arr.length - 1; //перейти на последнюю карточку
+      } else {
+        return prevIndex - 1; //уменьшить индекс на 1
+      }
+    });
   };
 
   if (!words) {
@@ -54,6 +69,8 @@ export default function Slider() {
         russian={object.russian}
         index={object.index}
         key={object.index}
+        //translated={translated}
+        //showTranslation={showTranslation}
       />
 
       <button className={style.btn_next} onClick={goToNextCard}>
