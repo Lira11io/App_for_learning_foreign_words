61cc8b4698dd06e3a8a171630435fc78c5b0bfec
Lira11io <Lira11pm@mail.ru>
2023-09-13 22:29:58 +0300

    загружены логотипы, исправлены компоненты card и slaider чтобы при перелистывании перевод скрывался

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,23 +1,6 @@
-import { useState } from "react";
 import style from "./card.module.scss";
 
 export default function Card(props) {
-  //задаем первоначальное состояние для кнопки, при котором перевод скрыт
-  const [showTranslation, setShowTranslation] = useState(false);
-  //добавляем состояние, которое означает, что перевод уже показан
-  const [translationShown, setTranslationShown] = useState(false);
-
-  //зададим функцию, вызываемую по клике на кнопку и передающую соответствующий перевод
-  const handleShowTranslation = () => {
-    if (!translationShown) {
-      setShowTranslation(true);
-      setTranslationShown(true);
-    } else {
-      setShowTranslation(false);
-      setTranslationShown(false);
-    }
-  };
-
   //возвращаю разметку карточки
   return (
     <>
@@ -25,10 +8,10 @@ export default function Card(props) {
         <div className={style.card}>
           <div className={style.card_word}>{props.english}</div>
           <div className={style.card_transcription}>{props.transcription}</div>
-          {showTranslation ? (
+          {props.showTranslation ? (
             <div
               className={style.card_translation}
-              onClick={handleShowTranslation}
+              onClick={props.handleShowTranslation}
             >
               {props.russian}
             </div>
@@ -36,7 +19,7 @@ export default function Card(props) {
             <div className={style.card_button}>
               <button
                 className={style.card_btn}
-                onClick={handleShowTranslation}
+                onClick={props.handleShowTranslation}
               >
                 Показать перевод
               </button>
